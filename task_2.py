584a32b650329652fce8fd60620e97d6f07bc075
miroslav2122 <miroslav211120008@gmail.com>
2024-10-23 16:29:57 +0000

    hhuhu

--- task_2.py
+++ task_2.py
@@ -1,4 +1,7 @@
-a = int(input( "Введите любой год"))
+mport numpy as np
+import constants as sv
+i
+= int(input( "Введите любой год"))
 if  a%4 == 0 or (a%100 == 0 and a%400 == 0);
     print("високоснвй")
 elif a%4 == 0;
--- task_3.py
+++ task_3.py
@@ -1,2 +1,20 @@
-a = input("Введите любой год: ")
-if year%4 == 
+import numpy as np
+x0 = 0
+v0 = 20
+y0 = 0
+
+d = 45
+r = np.radians(d)
+
+v0x = v0*np.cos(r)
+v0y = v0 * np.sin(r)
+
+g = 10
+
+t = np.linspace(0, 5, 100)
+
+x = x0 + v0y * t 
+y = y0 + v0y * t - 0.5 * g * (t ** 2)
+
+res = np.column_stack((t,x,y))
+print(res)
