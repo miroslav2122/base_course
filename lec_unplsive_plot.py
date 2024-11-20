eba54b55f7fb8576f54f04198978fec30b40edcf
miroslav2122 <miroslav211120008@gmail.com>
2024-11-20 16:00:14 +0000

    hghh

---
+++ lec_unplsive_plot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def circle_plotter(R=10):
+    x = np.arange(-2*R, 2*R, 0.1)
+    y = np.arange(-2*R, 2*R, 0.1)
+
+    # Переход
---
+++ task_4.py
@@ -0,0 +1,5 @@
+import numpy as np
+N = int(input('Введите элемент'))
+M = int(input('Введите элемент'))
+trigonometry_array = np.zeros((N, M))
+for i in range(N)
